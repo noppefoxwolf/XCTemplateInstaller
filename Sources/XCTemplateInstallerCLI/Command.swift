201eaf5bcb65129cca2ff9e2884aad4c5471655f
noppe <noppelabs@gmail.com>
2021-10-21 15:39:05 +0900

    support verbose

--- Sources/XCTemplateInstallerCLI/Command.swift
+++ Sources/XCTemplateInstallerCLI/Command.swift
@@ -7,17 +7,22 @@ struct XCTemplateInstaller: ParsableCommand {
     @Option(name: .shortAndLong, help: "The filepath of xctemplate.")
     var xctemplatePath: String?
     
-    let defaultTemplatePath = Bundle.bundle.path(forResource: "VIPER", ofType: "xctemplate")!
-    let destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application/"
+    @Flag(name: .shortAndLong, help: "Show all process description.")
+    var verbose: Bool = false
+    
+    var defaultTemplatePath = Bundle.bundle.path(forResource: "VIPER", ofType: "xctemplate")!
+    var destinationRelativePath = "/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates/iOS/Application/"
     
     mutating func run() throws {
         let fileManager = FileManager.default
         let templatePath = xctemplatePath ?? defaultTemplatePath
+        verbosePrint("Install template ->", templatePath)
         let templateFileName = URL(fileURLWithPath: templatePath).lastPathComponent
         
         let destinationPath = bash(command: "xcode-select", arguments: ["--print-path"])
             .appending(destinationRelativePath)
             .appending(templateFileName)
+        verbosePrint("Copy template to ->", destinationPath)
         
         if !fileManager.fileExists(atPath: destinationPath){
         
@@ -28,6 +33,9 @@ struct XCTemplateInstaller: ParsableCommand {
             
             print("✅ \(templateFileName) installed succesfully 🎉. Enjoy it 🙂")
         } else {
+            verbosePrint("Template already exists.")
+            
+            verbosePrint("Delete exist item.")
             try fileManager.removeItem(
                 atPath: destinationPath
             )
@@ -36,8 +44,13 @@ struct XCTemplateInstaller: ParsableCommand {
                 atPath: templatePath,
                 toPath: destinationPath
             )
-            print(destinationPath)
             print("✅ \(templateFileName) already exists. So has been replaced succesfully 🎉. Enjoy it 🙂")
+        }
+    }
+    
+    func verbosePrint(_ items: Any...) {
+        if verbose {
+            print(items)
         }
     }
 }
